304799089e927b5a7a0016a945a5f2c4a0e49990
Hugo Arts <hugo.yoshi@gmail.com>
2009-02-12 22:23:29 -0600

    modify init file to create event manager.
    Some modules rely on the existence of the manager, so it must be created before anything else can be imported

--- __init__.py
+++ __init__.py
@@ -1,3 +1,10 @@
 #! /usr/bin/env python
 
+#many modules depend on the event.manager existing, so do that first
 import event
+
+manager = event.manager = event.Manager()
+
+
+#import other modules
+import clock
