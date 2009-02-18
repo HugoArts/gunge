e5d3508cd58710d0ecb5c47ec6ec3bec565c1370
Hugo Arts <hugo.yoshi@gmail.com>
2009-02-18 02:37:18 -0600

    added sprite module to __init__.py. Not yet functional until event.py is updated.
    also added locals struct, and init function which creates a clock and display for you. All this and the event manager now reside in the locals struct

--- __init__.py
+++ __init__.py
@@ -2,13 +2,26 @@
 
 __all__ = ('event', 'clock', 'media', 'display')
 
+#struct that will contain some objects like the event manager, display, and clock
+class locals:
+    pass
+
 #many modules depend on the event.manager existing, so do that first
 import event
 
-manager = event.manager = event.Manager()
+locals.manager = event.manager = event.Manager()
 
 
 #import other modules
 import clock
 import media
 import display
+import sprite
+
+def init(display_size, caption, background=None, flags=0):
+    import pygame
+    if background is None:
+        background = pygame.Surface(display_size)
+    locals.display = display.Display(display_size, caption, background, flags)
+    locals.clock = clock.Clock(25)
+    return (locals.manager, locals.display, locals.clock)
